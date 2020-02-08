0d5c39758ee2ac25b2fa6a2e68ac503548a43bac
reflection4046 <60182620+reflection4046@users.noreply.github.com>
2020-02-08 18:45:45 +0600

    largestNumber

---
+++ LargestNumber.java
@@ -0,0 +1,22 @@
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+	    int n1 = 5;
+		int n3 = 8;
+		int n2 = 12;
+		if( n1 >= n2 && n1 >= n3)
+			
+            System.out.println(n1 + " is the largest number.");
+		
+        else if (n2 >= n1 && n2 >= n3)
+        	
+            System.out.println(n2 + " is the largest number.");
+		
+        else
+            System.out.println(n3 + " is the largest number.");
+    }
+
+	}
+
+

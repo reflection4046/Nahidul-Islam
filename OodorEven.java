0babfdbe4d19e45bf956a486f08763cd2c5c9ff2
reflection4046 <60182620+reflection4046@users.noreply.github.com>
2020-02-08 18:46:37 +0600

    Oodor Even

---
+++ OodorEven.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OodorEven {
+
+	public static void main(String[] args) {
+		
+	      Scanner s = new Scanner(System.in);
+	      
+	        System.out.print("Enter a number: ");
+	        
+	        int num = s.nextInt();
+	        
+	        if(num % 2 == 0)
+	            System.out.println(num + " is even");
+	        else
+	            System.out.println(num + " is odd");
+	    }
+	
+
+	}
+

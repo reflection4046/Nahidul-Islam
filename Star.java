9c1cc2d2be43303c69bdd0359fcd5435ccc6bcaf
reflection4046 <60182620+reflection4046@users.noreply.github.com>
2020-02-10 18:31:40 +0600

    star show

---
+++ Star.java
@@ -0,0 +1,21 @@
+
+public class Star {
+
+	public static void main(String[] args) {
+		 int M=1;
+		 
+			for (int i=0;i<=3;i++)
+			{
+				for (int j=i;j<M;j++)
+				{
+					System.out.print("*");
+				}    
+				    M=M+2;
+				    System.out.println();
+				   
+			}
+
+	}
+
+
+	}
